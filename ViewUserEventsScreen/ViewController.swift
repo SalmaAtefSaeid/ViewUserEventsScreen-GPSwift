b21c934eb4c8598744fdf773a3ef867895e67731
Salma Atef <salma.atef.ebrahim@gmail.com>
2019-05-30 13:48:41 +0200

    Initial Commit

---
+++ ViewUserEventsScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewUserEventsScreen
+//
+//  Created by Salma Atef Saeid on 5/30/19.
+//  Copyright © 2019 Salma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
